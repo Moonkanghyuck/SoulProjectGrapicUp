cd36552a310f984effde0b2884f05e0205c0e2fa
bossxz <bossxz@naver.com>
2022-06-25 11:49:31 +0900

    06/25_랜덤 몬스터 스포너 제작

--- Assets/Scripts/Monster/MonsterSpawner.cs
+++ Assets/Scripts/Monster/MonsterSpawner.cs
@@ -19,10 +19,22 @@ public class MonsterSpawner : MonoBehaviour
 
 	[SerializeField]
 	private Transform _player;
+	[SerializeField]
+	private PlayerStat _playerStat;
 	private List<IMonster> _monsters = new List<IMonster>();
 	private ItemPool _itemPool;
 	[SerializeField]
 	private GameObject _skeletonPrefeb;
+	[SerializeField]
+	private GameObject _skeletonMagePrefeb;
+	[SerializeField]
+	private GameObject _orcPrefeb;
+	[SerializeField]
+	private GameObject _ghoulPrefeb;
+	[SerializeField]
+	private GameObject _pumpkinPrefeb;
+	[SerializeField]
+	private GameObject _dragonPrefeb;
 	private int _monsterCount = 0;
 
 	public void Update()
@@ -82,16 +94,106 @@ public class MonsterSpawner : MonoBehaviour
 
 			IMonster monster = ItemPool.GetObject<Skeleton>();
 
-			if (monster == null)
+			float skeletonPercent = 100 - _playerStat.Level;
+			float skeletonMagePercent = _playerStat.Level + 10;
+			float goulPercent = _playerStat.Level + 30;
+			float orcPercent = _playerStat.Level + 20;
+			float pumpkinPercent = _playerStat.Level + 10;
+			float dragonPercent = _playerStat.Level;
+			float[] probs = new float[6] {skeletonPercent, skeletonMagePercent, goulPercent, orcPercent, pumpkinPercent, dragonPercent };
+			switch (Choose(probs))
 			{
-				monster = Instantiate(_skeletonPrefeb, Vector3.zero, Quaternion.identity, null).GetComponent<Skeleton>();
-				monster.GameObject.SetActive(false);
+				case 0:
+					SpawnSkeleton(spawnVector);
+					break;
+				case 1:
+					SpawnSkeletonMage(spawnVector);
+					break;
+				case 2:
+					SpawnGhoul(spawnVector);
+					break;
+				case 3:
+					SpawnOrc(spawnVector);
+					break;
+				case 4:
+					SpawnPumpkin(spawnVector);
+					break;
+				case 5:
+					SpawnDragon(spawnVector);
+					break;
 			}
-			monster.SetPos(spawnVector);
-			monster.Init();
-			AddCount();
-			_monsters.Add(monster);
 		}
+	}
+
+	private void SpawnSkeleton(Vector3 pos)
+	{
+		Spawn<Skeleton>(_skeletonPrefeb, pos);
+	}
+
+	private void SpawnSkeletonMage(Vector3 pos)
+	{
+		Spawn<SkeletonWizard>(_skeletonMagePrefeb, pos);
+	}
+
+	private void SpawnOrc(Vector3 pos)
+	{
+		Spawn<Orc>(_orcPrefeb, pos);
+	}
+
+	private void SpawnDragon(Vector3 pos)
+	{
+		Spawn<Dragon>(_dragonPrefeb, pos);
+	}
+
+	private void SpawnGhoul(Vector3 pos)
+	{
+		Spawn<Ghoul>(_ghoulPrefeb, pos);
+	}
+
+	private void SpawnPumpkin(Vector3 pos)
+	{
+		Spawn<Pumpkin>(_pumpkinPrefeb, pos);
+	}
+
+	private void Spawn<T>(GameObject prefeb, Vector3 pos) where T : IMonster
+	{
+		IMonster monster = ItemPool.GetObject<T>();
+
+		if (monster == null)
+		{
+			monster = Instantiate(prefeb, Vector3.zero, Quaternion.identity, null).GetComponent<T>();
+			monster.GameObject.SetActive(false);
+		}
+		monster.SetPos(pos);
+		monster.Init();
+		AddCount();
+		_monsters.Add(monster);
+	}
+
+	private int Choose(float[] probs)
+	{
+
+		float total = 0;
+
+		foreach (float elem in probs)
+		{
+			total += elem;
+		}
+
+		float randomPoint = Random.value * total;
+
+		for (int i = 0; i < probs.Length; i++)
+		{
+			if (randomPoint < probs[i])
+			{
+				return i;
+			}
+			else
+			{
+				randomPoint -= probs[i];
+			}
+		}
+		return probs.Length - 1;
 	}
 
 }
